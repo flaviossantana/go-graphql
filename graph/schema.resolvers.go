1078fef9e1a0d20c8179357da1544ed7fa0e9210
flavio-ss <9090flavio@gmail.com>
2021-10-27 20:58:11 -0300

    GRAPHQL - IMPLEMENTANDO QUERIES E PLAYGROUND

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,21 +6,52 @@ package graph
 import (
 	"context"
 	"fmt"
+	"math/rand"
 
 	"github.com/flaviossantana/go-graphql/graph/generated"
 	"github.com/flaviossantana/go-graphql/graph/model"
 )
 
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
-	panic(fmt.Errorf("not implemented"))
+	category := model.Category{
+		ID:          generateID(),
+		Name:        input.Name,
+		Description: &input.Description,
+	}
+
+	r.Categories = append(r.Categories, &category)
+
+	return &category, nil
+
 }
 
 func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCourse) (*model.Course, error) {
-	panic(fmt.Errorf("not implemented"))
+
+	course := model.Course{
+		ID:          generateID(),
+		Name:        input.Name,
+		Description: &input.Description,
+		Category:    findCategoryById(r, input.CategoryID),
+	}
+
+	r.Courses = append(r.Courses, &course)
+
+	return &course, nil
+
 }
 
 func (r *mutationResolver) CreateChapter(ctx context.Context, input model.NewChapter) (*model.Chapter, error) {
-	panic(fmt.Errorf("not implemented"))
+
+	chapter := model.Chapter{
+		ID:     generateID(),
+		Name:   input.Name,
+		Course: findCourseById(r, input.CourseID),
+	}
+
+	r.Chapters = append(r.Chapters, &chapter)
+
+	return &chapter, nil
+
 }
 
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
@@ -43,3 +74,27 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+func generateID() string {
+	return fmt.Sprintf("T%d", rand.Int())
+}
+
+func findCategoryById(r *mutationResolver, categoryID string) *model.Category {
+	var category *model.Category
+	for _, findCategory := range r.Categories {
+		if findCategory.ID == categoryID {
+			category = findCategory
+		}
+	}
+	return category
+}
+
+func findCourseById(r *mutationResolver, categoryID string) *model.Course {
+	var course *model.Course
+	for _, findCourse := range r.Courses {
+		if findCourse.ID == categoryID {
+			course = findCourse
+		}
+	}
+	return course
+}
